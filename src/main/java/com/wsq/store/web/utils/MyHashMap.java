e570e44ca97b6ecccc10bf6eb5f2b492c4ca6f0c
wushiquan <2794845600@qq.com>
2020-11-03 00:03:45 +0800

    myMap

---
+++ src/main/java/com/wsq/store/web/utils/MyHashMap.java
@@ -0,0 +1,19 @@
+package com.wsq.store.web.utils;
+
+/**
+ * 自己实现一个hashmap，注意如下几点
+ * 1.node数组的定义：node数组继承自Map.Entry，是一个映射
+ *  自定义实现------https://www.zhihu.com/people/cross-45-82
+ */
+public class MyHashMap {
+
+    /**
+     * node数组（主要是实现Map.Entry),作为一个键值对类型，便于存值
+     */
+
+
+
+    /**
+     *
+     */
+}
