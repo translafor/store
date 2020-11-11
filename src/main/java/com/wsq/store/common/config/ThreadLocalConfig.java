3ac70d5adc1e574e99f1ef29e62f9f2c7a214f2d
wushiquan <2794845600@qq.com>
2020-11-12 00:25:20 +0800

    myMap

---
+++ src/main/java/com/wsq/store/common/config/ThreadLocalConfig.java
@@ -0,0 +1,9 @@
+package com.wsq.store.common.config;
+
+
+public class ThreadLocalConfig {
+    /**
+     * threadLocal就是一个线程存一份本地变量
+     * https://www.cnblogs.com/fsmly/p/11020641.html
+     */
+}
