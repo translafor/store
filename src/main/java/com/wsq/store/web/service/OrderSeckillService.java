13268d66baf49b4a5e3c25e5e6bdfba58c4276d7
translafor <2794845600@qq.com>
2020-12-04 20:16:41 +0800

    商品秒杀

---
+++ src/main/java/com/wsq/store/web/service/OrderSeckillService.java
@@ -0,0 +1,14 @@
+package com.wsq.store.web.service;
+
+/**
+ * @description:
+ * @projectName:store
+ * @see:com.wsq.store.web.service
+ * @author:translafor
+ * @createTime:2020/12/4 20:08
+ * @version:1.0
+ */
+public class OrderSeckillService {
+    //商品秒杀：测试：JMeter  https://www.cnblogs.com/flysand/p/10737091.html
+    //https://www.cnblogs.com/jason9527/p/12538534.html
+}
