b307c28bce6235aba69307e6fba367fe45c3f486
translafor <2794845600@qq.com>
2021-09-01 16:14:58 +0800

    测试提交到master_1

--- src/main/java/com/wsq/store/web/controller/LoginController.java
+++ src/main/java/com/wsq/store/web/controller/LoginController.java
@@ -30,10 +30,11 @@ public class LoginController {
      * @param rps
      * @param rpo
      */
-    @GetMapping("/httttwot")
+    @GetMapping("/donotplease")
     public ResponseResult<?> userLogin(HttpServletRequest rps, HttpServletResponse rpo){
         System.out.println();
         try {
+            System.out.println("hello everyone");
             return ResponseResult.success(loginService.login(rps,rpo));
         }catch (UserNotifyException e){
             logger.error(e.getMessage(),e);
---
+++ src/main/java/com/wsq/store/web/controller/MasterNewClass.java
@@ -0,0 +1,9 @@
+package com.wsq.store.web.controller;
+
+/**
+ * @description: master测试新文件
+ * @author: frank.wu
+ * @create: 2021-09-01
+ */
+public class MasterNewClass {
+}
